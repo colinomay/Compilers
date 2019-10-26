6859732cf35f26fe9bf412707772f88c568c7549
colinomay <colin.may1@marist.edu>
2019-10-26 15:34:48 -0400

    Add files via upload

---
+++ Compiler/src/Compiler.java
@@ -0,0 +1,133 @@
+import java.io.*;
+import java.util.*;
+
+public class Compiler{
+    public static void main(String args[]) throws Exception {
+        System.out.println("Colin's Compiler");
+        File file = new File(args[0]);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String ln;
+        ArrayList<Lexeme> dictionary = createDictionary();
+        ArrayList<ArrayList<String>> programs = new ArrayList<ArrayList<String>>();
+        ArrayList<String> tokens = new ArrayList<String>();
+        ArrayList<String> lines = new ArrayList<String>();
+        String temp = "";
+        String match = "";
+        String match_name = "";
+        int position=0;
+        while ((ln = br.readLine()) != null) {
+            lines.add(ln);
+        }
+        System.out.println("INFO  Lexer - Lexing program " + (programs.size()+1) + "...");
+        for (int i = 0; i < lines.size(); i++) {
+            char[] line = lines.get(i).toString().toLowerCase().toCharArray();
+            System.out.println("Line Size: " + line.length);
+            for (int j = 0; j < line.length; j++) {
+                temp = temp + line[j];
+                System.out.println("Character: " +line[j]);
+                System.out.println("Temp: " + temp);
+                System.out.println("Current match: " +match);
+                if (Character.isWhitespace(line[j])) {
+                    System.out.println(temp.length());
+                    if(match.length() > 0) {
+                        tokens.add(match);
+                        System.out.println("DEBUG Lexer - " +  match_name + " [ " + match + " ] found at (" + i + ":" + (j -match.length()+1) + ")");
+                        j = j - (temp.length() - match.length());
+                    } else if(temp.length() > 1) {
+                        System.out.println("ERROR Lexer - Error:" + (i+1) + ":" + (j+1) + " Unrecognized Token: " + temp);
+                    }
+                } else if (temp.equals("$")) {
+                    tokens.add("$");
+                    System.out.println("DEBUG Lexer - EOP [ $ ] found at (" + (i+1) + ":" + (j -match.length()+1) + ")");
+                    programs.add(tokens);
+                    //for(int y = 0; y < tokens.size(); y++) {
+                        //System.out.print(tokens.get(y));
+                    //}
+                    tokens.clear();
+                }
+                else {
+                    for(int x = 0; x < dictionary.size(); x++) {
+                        if(temp.equals(dictionary.get(x).getValue())) {
+                            match = temp;
+                            {
+                                if(match.length() < 2)
+                                    position = j;
+                            }
+                            System.out.println("New Match: " +match);
+                            match_name = dictionary.get(x).getName();
+                        }
+                    }
+                    System.out.println(j);
+                    if(j+1 == line.length) {
+                        System.out.println("Token Added");
+                        tokens.add(match);
+                        System.out.println("DEBUG Lexer - " +  match_name + " [ " + match + " ] found at (" + (i+1) + ":" + (position+1) + ")");
+                        if(temp.length() != match.length())
+                            j = j - (temp.length() - match.length());
+                        match = "";
+                        temp = "";
+                    }
+
+                }
+            }
+        }
+    }
+
+    public static ArrayList<Lexeme> createDictionary() {
+        ArrayList<Lexeme> dictionary = new ArrayList<Lexeme>();
+        dictionary.add(new Lexeme("L_BRACE","{"));
+        dictionary.add(new Lexeme("R_BRACE","}"));
+        dictionary.add(new Lexeme("PRINT_OP","print"));
+        dictionary.add(new Lexeme("L_PAREN","("));
+        dictionary.add(new Lexeme("R_BRACE",")"));
+        dictionary.add(new Lexeme("ASSIGN_OP","="));
+        dictionary.add(new Lexeme("WHILE_OP","while"));
+        dictionary.add(new Lexeme("IF_OP","if"));
+        dictionary.add(new Lexeme("QUOTE","'"));
+        dictionary.add(new Lexeme("INT_TYPE","int"));
+        dictionary.add(new Lexeme("STRING_TYPE","string"));
+        dictionary.add(new Lexeme("BOOLEAN_TYPE","boolean"));
+        dictionary.add(new Lexeme("CHAR","a"));
+        dictionary.add(new Lexeme("CHAR","b"));
+        dictionary.add(new Lexeme("CHAR","c"));
+        dictionary.add(new Lexeme("CHAR","d"));
+        dictionary.add(new Lexeme("CHAR","e"));
+        dictionary.add(new Lexeme("CHAR","f"));
+        dictionary.add(new Lexeme("CHAR","g"));
+        dictionary.add(new Lexeme("CHAR","h"));
+        dictionary.add(new Lexeme("CHAR","i"));
+        dictionary.add(new Lexeme("CHAR","j"));
+        dictionary.add(new Lexeme("CHAR","k"));
+        dictionary.add(new Lexeme("CHAR","l"));
+        dictionary.add(new Lexeme("CHAR","m"));
+        dictionary.add(new Lexeme("CHAR","n"));
+        dictionary.add(new Lexeme("CHAR","o"));
+        dictionary.add(new Lexeme("CHAR","p"));
+        dictionary.add(new Lexeme("CHAR","q"));
+        dictionary.add(new Lexeme("CHAR","r"));
+        dictionary.add(new Lexeme("CHAR","s"));
+        dictionary.add(new Lexeme("CHAR","t"));
+        dictionary.add(new Lexeme("CHAR","u"));
+        dictionary.add(new Lexeme("CHAR","v"));
+        dictionary.add(new Lexeme("CHAR","w"));
+        dictionary.add(new Lexeme("CHAR","x"));
+        dictionary.add(new Lexeme("CHAR","y"));
+        dictionary.add(new Lexeme("CHAR","z"));
+        dictionary.add(new Lexeme("DIGIT","0"));
+        dictionary.add(new Lexeme("DIGIT","1"));
+        dictionary.add(new Lexeme("DIGIT","2"));
+        dictionary.add(new Lexeme("DIGIT","3"));
+        dictionary.add(new Lexeme("DIGIT","4"));
+        dictionary.add(new Lexeme("DIGIT","5"));
+        dictionary.add(new Lexeme("DIGIT","6"));
+        dictionary.add(new Lexeme("DIGIT","7"));
+        dictionary.add(new Lexeme("DIGIT","8"));
+        dictionary.add(new Lexeme("DIGIT","9"));
+        dictionary.add(new Lexeme("BOOLOP","=="));
+        dictionary.add(new Lexeme("BOOLVAL","false"));
+        dictionary.add(new Lexeme("BOOLVAL","true"));
+        dictionary.add(new Lexeme("INTOP","+"));
+
+        return dictionary;
+    }
+}
---
+++ Compiler/src/Lexeme.java
@@ -0,0 +1,22 @@
+public class Lexeme {
+    String name;
+    String value;
+
+    public Lexeme(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getValue() {
+        return value;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
